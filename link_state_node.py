75ef642f6f06f6ff404863f413ed0a85e1cc665a
Alexis Robles <alexis.robles49@gmail.com>
2023-03-03 13:28:59 -0600

    All but 1 test cases PASSED

--- link_state_node.py
+++ link_state_node.py
@@ -13,13 +13,13 @@ class Link_State_Node(Node):
         self.graph = {}
 
         # Links
-        self.neighbor_links = {}
+        self.links = {}
 
     def __str__(self):
         """
         Return a string with the current node's ID, graph, and links
         """
-        return f"Node {self.id}: {self.neighbor_links}"
+        return f"Node {self.id}: {self.links}"
 
     def update_graph(self, latency, node1, node2):
 
@@ -28,21 +28,22 @@ class Link_State_Node(Node):
 
         # If latency is -1, remove the link from the graph
         # (i.e. remove each node from each other's list of neighbors)
-        if latency == -1:
+        # if latency == -1 and link in self.graph:
+        if latency == -1 and link in self.graph:
 
-            # Try to remove
-            try:
-                del self.graph[link]
-                print(link)
-            except:
-                pass
+            # Remove link
+            del self.graph[link]
 
         # Latency is not -1, so link will be updated
         else:
 
-            # Add link to graph with given latency for other node
-            self.graph[link] = latency
+            # Make sure you don't add an expired link
+            if latency != -1:
 
+                # Add link to graph with given latency for other node
+                self.graph[link] = latency
+
+        # print("Updated Graph:", self.graph)
 
     def link_has_been_updated(self, neighbor, latency):
         """
@@ -50,16 +51,22 @@ class Link_State_Node(Node):
         """
 
         # Update the graph with the given neighbor and latency
+        # print("Nodes", self.id, neighbor, "with updated link of latency", latency)
         self.update_graph(latency, self.id, neighbor)
 
         # Create a link as a frozenset between itself and its neighbor
         link = frozenset({self.id, neighbor})
 
         # If it's not a new link
-        if link in self.neighbor_links:
+        if link in self.links:
 
-            # Set the current sequence number to the one the node knows from self.neighbor_links
-            seq = self.neighbor_links[link]["seq"] + 1
+            # Set the current sequence number to the one the node knows from self.links
+            seq = self.links[link]["seq"] + 1
+
+            # Delete the link
+            # (this doesn't change anything for some reason)
+            if latency == -1:
+                del self.links[link]
 
         # Otherwise, if the link is new
         else:
@@ -69,17 +76,19 @@ class Link_State_Node(Node):
 
             # Send current node's links to new neighbor to properly update the
             # new neighbor with the latest sequence numbers for the links
-            for link_info in self.neighbor_links.values():
+            for link_info in self.links.values():
                 self.send_to_neighbor(neighbor, json.dumps(link_info))
+
+        # print("Current links:", self.links)
 
         # Create message according to instructions
         msg = {"src": self.id,
-               "dst": neighbor,
-               "lat": latency,
-               "seq": seq}
+                "dst": neighbor,
+                "lat": latency,
+                "seq": seq}
 
-        # Add the message to self.neighbor_links for the current link
-        self.neighbor_links[link] = msg
+        # Add the message to self.links for the current link
+        self.links[link] = msg
 
         # Send the message to all its neighbors
         self.send_to_neighbors(json.dumps(msg))
@@ -101,19 +110,30 @@ class Link_State_Node(Node):
         src = msg["src"]
         dst = msg["dst"]
         lat = msg["lat"]
+        # print('\nNode', self.id, 'receiving message for link', src, dst, 'with latency', lat)
 
         # If link is known
-        if link in self.neighbor_links:
+        if link in self.links:
 
             # Check if it's an old seq number (i.e. simulation bug)
-            if seq < self.neighbor_links[link]["seq"]:
+            if seq < self.links[link]["seq"]:
+
+                # Send the newer message back to the node who sent the older message
+                self.send_to_neighbor(src, json.dumps(self.links[link]))
+
+            elif seq > self.links[link]["seq"]:
+
+                # Update self.links
+                self.links[link] = msg
 
                 # Convert msg back to string with JSON
                 msg = json.dumps(msg)
 
-                # Send the newer message back to the node who sent the older message
-                self.send_to_neighbor(src, msg)
-                return
+                # Update the graph
+                self.update_graph(lat, src, dst)
+
+                # Pass the message along to the node's neighbors
+                self.send_to_neighbors(msg)
 
             else:
 
@@ -123,8 +143,8 @@ class Link_State_Node(Node):
         # Otherwise, if the link is not known or if it's known but a new seq number
         else:
 
-            # Update self.neighbor_links
-            self.neighbor_links[link] = msg
+            # Update self.links
+            self.links[link] = msg
 
             # Convert msg back to string with JSON
             msg = json.dumps(msg)
@@ -211,14 +231,22 @@ class Link_State_Node(Node):
         # Build the path from start to end by following the predecessor links
         path = []
         curr_node = destination
+
+        # print()
+        # print(self.graph)
+        # print()
+        # print(self.links)
+        # print()
+
         while curr_node is not None:
             path.append(curr_node)
+            # print(self.graph)
+            # print('Node', self.id, 'w/ Pred:', predecessor[curr_node])
+            # print()
             curr_node = predecessor[curr_node]
 
         # Reverse path so that it goes source -> destination
         path = path[::-1]
-
-        print('Path:', str(path))
 
         # If we didn't reach the end node, there's no path
         if path[0] != self.id:
